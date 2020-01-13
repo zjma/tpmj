8cbc8fc777416e448ad467d1566462bcca378fa8
zjma <meshiadia@gmail.com>
2020-01-12 16:49:59 -0800

    mode dialog as a component

--- webcli/src/App.vue
+++ webcli/src/App.vue
@@ -1,25 +1,8 @@
 <template>
   <v-app>
       <login-dialog :active="IsLoginDialogActive" :default-user-name="UserName" @finished="onLoginFinished" />
-      <v-dialog v-model="ShowModeDialog" persistent max-width="600px">
-        <v-card>
-          <v-card-title>
-            <span class="headline">Choose Mode</span>
-          </v-card-title>
-
-          <v-container>
-              <v-radio-group v-model="Mode">
-                  <v-radio label="Play" value="Play"></v-radio>
-                  <v-radio label="Observe" value="Observe"></v-radio>
-              </v-radio-group>
-          </v-container>
-          <v-card-actions>
-            <v-btn color="blue darken-1" text @click="onBackPressed()">Back</v-btn>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="onModeChosen()">Next</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
+      <mode-dialog :active="IsModeDialogActive" :default-mode="mode" @play-selected="onPlayModeSelected" @observe-selected="onObserveModeSelected" @cancelled="onModeSelectionCancelled" />
+      <!-- <observer-waiting-dialog :active="IsObserverWaitingDialogActive" @cancelled="onObserverWaitingCancelled" /> -->
       <v-dialog v-model="ShowObserverWaitingDialog" persistent max-width="600px">
         <v-card>
           <v-card-title>
@@ -33,11 +16,7 @@
           </v-card-actions>
         </v-card>
       </v-dialog>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           src="@/assets/logo.png"
@@ -79,17 +58,21 @@
 <script>
 import HelloWorld from './components/HelloWorld';
 import LoginDialog from './LoginDialog.vue';
+import ModeDialog from './ModeDialog.vue';
+// import ObserverWaitingDialog from './ObserverWaitingDialog.vue';
 export default {
   name: 'App',
 
   components: {
-    'login-dialog':LoginDialog,
+    'login-dialog'              : LoginDialog,
+    'mode-dialog'               : ModeDialog,
+    // 'observer-waiting-dialog'   : ObserverWaitingDialog,
     HelloWorld
   },
 
   data: () => ({
     UserName: 'NoName',
-    Mode    : 'Play',
+    mode    : 'Play',
     State   : 'UserLoggingIn'
   }),
   computed: {
@@ -97,7 +80,7 @@ export default {
         return this.State == 'UserLoggingIn'
     },
 
-    ShowModeDialog: function(){
+    IsModeDialogActive: function(){
         return this.State == 'UserSelectingMode'
     },
 
@@ -105,33 +88,41 @@ export default {
         return this.State == 'ObserverWaitingForGame'
     }
   },
-  methods: {
-    onBackPressed(){
-        this.State = 'UserLoggingIn'
-    },
-    onModeChosen(){
-        if (this.Mode == 'Observe') {
-            this.State = 'ObserverWaitingForGame'
-            setInterval(function(){
-                // this.$http.post('/tpmj',{Action:'GetObservableGameID'}).then(function(res){
-                //     window.console.log(res.json);
-                // },function(){
-                //     window.console.log('Failed');
-                // });
-                window.console.log('Working.');
-            }, 1000)
-        } else {
+    methods: {
+        onPlayModeSelected : function(){
+            this.mode = 'Play';
+            this.State = 'PlayerWaitingForMatch';
+        },
+        onObserveModeSelected : function(){
+            this.mode = 'Observe';
+            this.State = 'ObserverWaitingForGame';
+        },
+        onModeSelectionCancelled : function(){
+            this.State = 'UserLoggingIn';
+        },
+        onModeChosen(){
+            if (this.Mode == 'Observe') {
+                this.State = 'ObserverWaitingForGame'
+                setInterval(function(){
+                    // this.$http.post('/tpmj',{Action:'GetObservableGameID'}).then(function(res){
+                    //     window.console.log(res.json);
+                    // },function(){
+                    //     window.console.log('Failed');
+                    // });
+                    window.console.log('Working.');
+                }, 1000)
+            } else {
+                this.State = 'UserSelectingMode'
+            }
+        },
+        onObserverWaitingCanceled(){
+            this.State = 'UserSelectingMode'
+        },
+        onLoginFinished(userName){
+            window.console.log(userName);
+            this.UserName = userName
             this.State = 'UserSelectingMode'
         }
-    },
-    onObserverWaitingCanceled(){
-        this.State = 'UserSelectingMode'
-    },
-    onLoginFinished(userName){
-        window.console.log(userName);
-        this.UserName = userName
-        this.State = 'UserSelectingMode'
     }
-  }
 };
 </script>
---
+++ webcli/src/ModeDialog.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-dialog v-model="active" persistent max-width="600px">
+        <v-card>
+            <v-card-title>
+                <span class="headline">Choose Mode</span>
+            </v-card-title>
+            <v-container>
+                <v-radio-group v-model="mode">
+                    <v-radio label="Play" value="Play"></v-radio>
+                    <v-radio label="Observe" value="Observe"></v-radio>
+                </v-radio-group>
+            </v-container>
+            <v-card-actions>
+                <v-btn color="blue darken-1" text @click="onBack()">Back</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="onNext()">Next</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+    name: 'ModeDialog',
+    props : {
+        active:Boolean,
+        defaultMode:String
+    },
+    data : function(){
+        return {
+            mode : this.defaultMode
+        };
+    },
+    methods: {
+        onBack : function(){
+            this.$emit('cancelled');
+        },
+        onNext : function(){
+
+        }
+    }
+}
+</script>
