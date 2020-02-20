7a0c37f12215975b79f453208aef3da0fcb26707
zjma <zjma@users.noreply.github.com>
2020-02-19 22:50:08 -0800

    Frontend unittests (#19)
    * 3Xn2 unittests
    
    * dead code cleaning
    
    * code layout change
    
    * fix travis
    
    * put a failing test to validate travis
    
    * trigger ci

--- webcli/src/PlayerAreaStyling.js
+++ webcli/src/PlayerAreaStyling.js
@@ -1,19 +1,3 @@
-export function getDimensions(width, height) {
-    var ret = Object()
-    ret.tileHeight = height / 10
-    ret.tileWidth = width / 19
-    ret.riverTop = 0
-    ret.riverLeft = ret.tileWidth*6.5
-    ret.MountainTop = ret.tileHeight*4
-    ret.MountainLeft = 0
-    ret.HandTop = ret.tileHeight*7
-    ret.HandLeft = ret.tileWidth*2
-    ret.SetTop = ret.tileHeight*9
-    ret.SetWidth = ret.tileWidth*3+ret.tileHeight
-    ret.SetHeight = ret.tileWidth*2
-    return ret
-}
-
 var TileGroupChars = ['🀇','🀈','🀉','🀊','🀋','🀌','🀍','🀎','🀏','🀐','🀑','🀒','🀓','🀔','🀕','🀖','🀗','🀘','🀙','🀚','🀛','🀜','🀝','🀞','🀟','🀠','🀡','🀀','🀁','🀂','🀃','🀆','🀅','🀥']
 var SeatChars = ['東','南','西','北']
 
--- webcli/src/util.js
+++ webcli/src/util.js
@@ -1,3 +1,7 @@
+/*
+Global helper functions.
+*/
+
 export function randInt(start, until) {
     var width = Math.max(0, until - start)
     return start + Math.floor(Math.random()*width)
