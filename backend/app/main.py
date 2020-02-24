2514e4a8c53a563262e5607b6e80cd15638066fa
zjma <meshiadia@gmail.com>
2020-02-23 22:26:01 -0800

    api server now serves version

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,12 @@
+import os
+import pathlib
 import logging
 from flask import Flask,request,jsonify
 from flask_cors import CORS
 from Host import Host
+
+with open(str(pathlib.Path(__file__).parent.parent.parent.joinpath('Version'))) as f:
+    Version = f.read().strip()
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -13,16 +18,24 @@ host = Host()
 
 @app.route("/tpmj", methods=["POST"])
 def tpmj():
-	try:
-		req = request.get_json(force=True,silent=True)
-		if req == None: req = {}
-		response = host.handle(req)
-		return jsonify(response)
-	except Exception as e:
-		# Log exception raised while handling the request.
-		logger.exception('Handling exception.')
-		return jsonify({'Processed':0})
+    try:
+        req = request.get_json(force=True,silent=True)
+        if req == None: req = {}
+        response = host.handle(req)
+        return jsonify(response)
+    except Exception as e:
+        # Log exception raised while handling the request.
+        logger.exception('Handling exception.')
+        return jsonify({'Processed':0})
+
+@app.route("/about", methods=["GET"])
+def about():
+    return jsonify({
+        'Version':Version,
+    })
 
 if __name__ == "__main__":
-    # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=8080)
+    isDevelopment = 'TPMJ_BACKEND_PRODUCTION' not in os.environ
+    if isDevelopment:
+        Version+='*'
+    app.run(host='0.0.0.0', debug=isDevelopment, port=8080)
