4aace93e02124d3d9cb6e3d6abc41ca746a31a6f
zjma <meshiadia@gmail.com>
2020-08-16 21:15:12 -0700

    update frontend port

--- webcli/app.js
+++ webcli/app.js
@@ -3,7 +3,7 @@ const app = express();
 const router = express.Router();
 
 const path = __dirname + '/2pmj/';
-const port = 80;
+const port = 8081;
 
 router.use(function (req,res,next) {
   console.log('/' + req.method);
@@ -13,5 +13,5 @@ router.use(function (req,res,next) {
 app.use('/2pmj', express.static(path));
 app.use('/', router);
 app.listen(port, function () {
-  console.log('Example app listening on port 80!')
+  console.log('Example app listening on port 8081!')
 })
